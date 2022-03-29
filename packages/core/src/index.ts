b1e47ac111cdd8c51ad28f80e19294b767da461e
Andrey Kondalov <andrey@kondalov.com>
2022-03-30 00:28:36 +0400

    feat(core): instance exported

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,8 @@
+import styled from '@emotion/styled';
+import { css, keyframes, useTheme, withTheme, ThemeContext, CacheProvider, withEmotionCache, ClassNames } from '@emotion/react';
+
+export { styled, css, keyframes, useTheme, withEmotionCache, withTheme, ThemeContext, CacheProvider, ClassNames };
+
 export { ThemeProvider } from './ThemeProvider';
 export { defaultTheme } from './defaultTheme';
 export { globalStyles } from './globalStyles';
