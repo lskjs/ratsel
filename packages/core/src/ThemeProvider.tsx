4d3b060bbc94522837e2b8e379072a4bbab33611
Andrey Kondalov <andrey@kondalov.com>
2022-03-31 01:05:01 +0400

    fix(core): remove logs & globalStyles prop

--- packages/core/src/ThemeProvider.tsx
+++ packages/core/src/ThemeProvider.tsx
@@ -1,28 +1,30 @@
 import React, { useMemo } from 'react';
-import { ThemeProvider as EmotionThemeProvider, Global } from '@emotion/react';
+import { ThemeProvider as EmotionThemeProvider, Global, SerializedStyles } from '@emotion/react';
 import type { FC } from 'react';
 import type { Theme } from '@emotion/react';
-import { globalStyles } from './globalStyles';
+import { globalStyles as defaultGlobalStyles } from './globalStyles';
 import { defaultTheme } from './defaultTheme';
 
 interface ThemeProviderProps {
   theme?: Partial<Theme> | ((outerTheme: Theme) => Theme);
+  globalStyles?: SerializedStyles;
 }
 
-export const ThemeProvider: FC<ThemeProviderProps> = ({ children, theme }) => {
-  const memoizedTheme = useMemo(
-    () => {
-      let composedTheme = theme || defaultTheme;
-      if (theme && typeof theme === 'function') {
-        composedTheme = theme(defaultTheme);
-      }
-      return (parentTheme: Theme) => ({
-        ...parentTheme,
-        [Symbol.for('ratsel')]: composedTheme,
-      });
-    },
-    [theme],
-  );
+export const ThemeProvider: FC<ThemeProviderProps> = ({
+  children,
+  theme,
+  globalStyles = defaultGlobalStyles
+}) => {
+  const memoizedTheme = useMemo(() => {
+    let composedTheme = theme || defaultTheme;
+    if (theme && typeof theme === 'function') {
+      composedTheme = theme(defaultTheme);
+    }
+    return (parentTheme: Theme) => ({
+      ...parentTheme,
+      [Symbol.for('ratsel')]: composedTheme,
+    });
+  }, [theme]);
 
   return (
     <EmotionThemeProvider theme={memoizedTheme}>
--- packages/core/src/globalStyles.ts
+++ packages/core/src/globalStyles.ts
@@ -1,37 +1,34 @@
 import { css, Theme } from '@emotion/react';
 
-export const globalStyles = (theme: Theme) => {
-  console.log(theme);
-  return css`
-    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap');
+export const globalStyles = (theme: Theme) => css`
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap');
 
-    body {
-      -webkit-font-smoothing: antialiased;
-      -moz-osx-font-smoothing: grayscale;
-    }
+  body {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
 
-    body,
-    button,
-    a {
-      font-family: ${theme[Symbol.for('ratsel')].fonts.main};
-    }
+  body,
+  button,
+  a {
+    font-family: ${theme[Symbol.for('ratsel')].fonts.main};
+  }
 
-    h1,
-    h2,
-    h3,
-    h4,
-    h5,
-    h6 {
-      font-family: ${theme[Symbol.for('ratsel')].fonts.headings};
-    }
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: ${theme[Symbol.for('ratsel')].fonts.headings};
+  }
 
-    pre,
-    code {
-      font-family: ${theme[Symbol.for('ratsel')].fonts.monospace};
-    }
+  pre,
+  code {
+    font-family: ${theme[Symbol.for('ratsel')].fonts.monospace};
+  }
 
-    * {
-      box-sizing: border-box;
-    }
-  `;
-}
+  * {
+    box-sizing: border-box;
+  }
+`;
