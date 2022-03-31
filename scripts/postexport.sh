d2bcd102a79420bea09615616cc6c422a046d81b
Andrey Kondalov <andrey@kondalov.com>
2022-03-31 18:04:23 +0400

    fix(docs): try relative path

--- scripts/postexport.sh
+++ scripts/postexport.sh
@@ -2,8 +2,8 @@
 
 sed -i '' 's/_cosmos.ico/cosmos.ico/g' docs/index.html
 sed -i '' 's/_playground.js/playground.js/g' docs/index.html
-sed -i '' 's/\/_renderer.html/renderer.html/g' docs/index.html
-sed -i '' 's/\/main.js/main.js/g' docs/_renderer.html
+sed -i '' 's/\/_renderer.html/.\/renderer.html/g' docs/index.html
+sed -i '' 's/\/main.js/.\/main.js/g' docs/_renderer.html
 mv docs/_playground.js docs/playground.js
 mv docs/_cosmos.ico docs/cosmos.ico
 mv docs/_renderer.html docs/renderer.html
