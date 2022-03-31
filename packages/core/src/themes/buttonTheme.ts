c7258e42cc0f29cb4ebfe1cebe58f952cfe33ba8
Andrey Kondalov <andrey@kondalov.com>
2022-03-31 19:41:38 +0400

    feat(core): update buttonTheme

--- packages/core/src/themes/buttonTheme.ts
+++ packages/core/src/themes/buttonTheme.ts
@@ -17,12 +17,22 @@ export interface ButtonThemePaddings {
   verticalIcon: string;
 }
 
+export interface ButtonThemeSpinner {
+  background: string;
+  accent: string;
+  inverseBackground: string;
+  inverseAccent: string;
+  success: string;
+  error: string;
+}
+
 export interface ButtonTheme {
   fontWeight: number;
   fontSize: string;
   focusShadow: string;
   borderColor: string;
   paddings: ButtonThemePaddings;
+  spinner: ButtonThemeSpinner;
   variants: {
     [variant: string]: ButtonThemeVariant;
   };
@@ -33,6 +43,14 @@ export const buttonTheme: ButtonTheme = {
   fontSize: '13px',
   focusShadow: '0 0 4px hsl(160, 76%, 52%)',
   borderColor: 'hsl(151, 49.3%, 46.5%)',
+  spinner: {
+    background: 'hsl(120, 8%, 95%)',
+    accent: 'hsl(151, 49.3%, 46.5%)',
+    inverseBackground: 'rgba(255, 255, 255, 0.1)',
+    inverseAccent: '#ffffff',
+    success: 'hsl(151, 49.3%, 46.5%)',
+    error: 'hsl(358, 75.0%, 59.0%)',
+  },
   paddings: {
     base: '24px',
     icon: '8px',
