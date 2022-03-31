cf33b8bdd995516fa7a445d42a3fae145fb5b0f2
Andrey Kondalov <andrey@kondalov.com>
2022-03-31 12:48:35 +0400

    fix(core): add font weight 600

--- packages/core/src/globalStyles.ts
+++ packages/core/src/globalStyles.ts
@@ -2,7 +2,7 @@ import { css } from '@emotion/react';
 import { Theme } from './themes/theme';
 
 export const globalStyles = (theme: Theme) => css`
-  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap');
 
   body {
     -webkit-font-smoothing: antialiased;
