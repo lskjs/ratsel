0297e1b4e667bcfbf4786b56c11ff0f6c12ff193
Andrey Kondalov <andrey@kondalov.com>
2022-03-31 10:29:43 +0400

    fix(core): styled template theme override

--- packages/core/src/styled.ts
+++ packages/core/src/styled.ts
@@ -1,15 +1,154 @@
 import emotionStyled from '@emotion/styled';
 import {
   withTheme as emotionWithTheme,
-  useTheme as emotionUseTheme,
+  useTheme as emotionUseTheme
 } from '@emotion/react';
 
 import type {
-  CreateStyled as BaseCreateStyled,
-  CreateStyledComponent,
-} from '@emotion/styled/types/base';
-import type { DistributiveOmit, PropsOf } from '@emotion/react';
+  StyledOptions,
+} from '@emotion/styled';
+import type {
+  DistributiveOmit,
+  PropsOf,
+  Interpolation,
+  ComponentSelector,
+} from '@emotion/react';
 import type { InnerTheme } from './themes/theme';
+import { FilteringStyledOptions } from '@emotion/styled/types/base';
+
+export interface StyledComponent<
+  ComponentProps extends {},
+  SpecificComponentProps extends {} = {},
+  JSXProps extends {} = {}
+> extends React.FC<ComponentProps & SpecificComponentProps & JSXProps>,
+    ComponentSelector {
+  withComponent<C extends React.ComponentClass<React.ComponentProps<C>>>(
+    component: C
+  ): StyledComponent<
+    ComponentProps & PropsOf<C>,
+    {},
+    { ref?: React.Ref<InstanceType<C>> }
+  >
+  withComponent<C extends React.ComponentType<React.ComponentProps<C>>>(
+    component: C
+  ): StyledComponent<ComponentProps & PropsOf<C>>
+  withComponent<Tag extends keyof JSX.IntrinsicElements>(
+    tag: Tag
+  ): StyledComponent<ComponentProps, JSX.IntrinsicElements[Tag]>
+}
+
+export interface CreateStyledComponent<
+  ComponentProps extends {},
+  SpecificComponentProps extends {} = {},
+  JSXProps extends {} = {}
+> {
+  <AdditionalProps extends {} = {}>(
+    ...styles: Array<
+      Interpolation<
+        ComponentProps &
+          SpecificComponentProps &
+          AdditionalProps & { theme: InnerTheme }
+      >
+    >
+  ): StyledComponent<
+    ComponentProps & AdditionalProps,
+    SpecificComponentProps,
+    JSXProps
+  >
+
+  (
+    template: TemplateStringsArray,
+    ...styles: Array<
+      Interpolation<ComponentProps & SpecificComponentProps & { theme: InnerTheme }>
+    >
+  ): StyledComponent<ComponentProps, SpecificComponentProps, JSXProps>
+
+  <AdditionalProps extends {}>(
+    template: TemplateStringsArray,
+    ...styles: Array<
+      Interpolation<
+        ComponentProps &
+          SpecificComponentProps &
+          AdditionalProps & { theme: InnerTheme }
+      >
+    >
+  ): StyledComponent<
+    ComponentProps & AdditionalProps,
+    SpecificComponentProps,
+    JSXProps
+  >
+}
+
+export interface BaseCreateStyled {
+  <
+    C extends React.ComponentClass<React.ComponentProps<C>>,
+    ForwardedProps extends keyof React.ComponentProps<C> = keyof React.ComponentProps<C>
+  >(
+    component: C,
+    options: FilteringStyledOptions<React.ComponentProps<C>, ForwardedProps>
+  ): CreateStyledComponent<
+    Pick<PropsOf<C>, ForwardedProps> & {
+      theme?: InnerTheme
+    },
+    {},
+    {
+      ref?: React.Ref<InstanceType<C>>
+    }
+  >
+
+  <C extends React.ComponentClass<React.ComponentProps<C>>>(
+    component: C,
+    options?: StyledOptions<React.ComponentProps<C>>
+  ): CreateStyledComponent<
+    PropsOf<C> & {
+      theme?: InnerTheme
+    },
+    {},
+    {
+      ref?: React.Ref<InstanceType<C>>
+    }
+  >
+
+  <
+    C extends React.ComponentType<React.ComponentProps<C>>,
+    ForwardedProps extends keyof React.ComponentProps<C> = keyof React.ComponentProps<C>
+  >(
+    component: C,
+    options: FilteringStyledOptions<React.ComponentProps<C>, ForwardedProps>
+  ): CreateStyledComponent<
+    Pick<PropsOf<C>, ForwardedProps> & {
+      theme?: InnerTheme
+    }
+  >
+
+  <C extends React.ComponentType<React.ComponentProps<C>>>(
+    component: C,
+    options?: StyledOptions<React.ComponentProps<C>>
+  ): CreateStyledComponent<
+    PropsOf<C> & {
+      theme?: InnerTheme
+    }
+  >
+
+  <
+    Tag extends keyof JSX.IntrinsicElements,
+    ForwardedProps extends keyof JSX.IntrinsicElements[Tag] = keyof JSX.IntrinsicElements[Tag]
+  >(
+    tag: Tag,
+    options: FilteringStyledOptions<JSX.IntrinsicElements[Tag], ForwardedProps>
+  ): CreateStyledComponent<
+    { theme?: InnerTheme; as?: React.ElementType },
+    Pick<JSX.IntrinsicElements[Tag], ForwardedProps>
+  >
+
+  <Tag extends keyof JSX.IntrinsicElements>(
+    tag: Tag,
+    options?: StyledOptions<JSX.IntrinsicElements[Tag]>
+  ): CreateStyledComponent<
+    { theme?: InnerTheme; as?: React.ElementType },
+    JSX.IntrinsicElements[Tag]
+  >
+}
 
 export type StyledTags = {
   [Tag in keyof JSX.IntrinsicElements]: CreateStyledComponent<
@@ -22,8 +161,6 @@ export type StyledTags = {
 }
 
 export interface CreateStyled extends BaseCreateStyled, StyledTags {}
-
-
 
 export type withThemeType = <
   C extends React.ComponentType<React.ComponentProps<C>>
