7438abe303db3580367ca537732c3ce3e772fc99
Andrey Kondalov <andrey@kondalov.com>
2022-04-14 17:41:23 +0400

    fix(table): export defaultComponents ka-table

--- packages/table/src/index.ts
+++ packages/table/src/index.ts
@@ -1,1 +1,117 @@
+import CellComponent from 'ka-table/Components/CellComponent/CellComponent';
+import CellEditor from 'ka-table/Components/CellEditor/CellEditor';
+import CellEditorBoolean from 'ka-table/Components/CellEditorBoolean/CellEditorBoolean';
+import CellEditorDataType from 'ka-table/Components/CellEditorDataType/CellEditorDataType';
+import CellEditorDate from 'ka-table/Components/CellEditorDate/CellEditorDate';
+import CellEditorNumber from 'ka-table/Components/CellEditorNumber/CellEditorNumber';
+import CellEditorState from 'ka-table/Components/CellEditorState/CellEditorState';
+import CellEditorString from 'ka-table/Components/CellEditorString/CellEditorString';
+import CellEditorValidation from 'ka-table/Components/CellEditorValidation/CellEditorValidation';
+import CellEditorValidationMessage from 'ka-table/Components/CellEditorValidationMessage/CellEditorValidationMessage';
+import CellText from 'ka-table/Components/CellText/CellText';
+import ColGroup from 'ka-table/Components/ColGroup/ColGroup';
+import DataAndDetailsRows from 'ka-table/Components/DataAndDetailsRows/DataAndDetailsRows';
+import DataRow from 'ka-table/Components/DataRow/DataRow';
+import DataRowContent from 'ka-table/Components/DataRowContent/DataRowContent';
+import DetailsRow from 'ka-table/Components/DetailsRow/DetailsRow';
+import EmptyCells from 'ka-table/Components/EmptyCells/EmptyCells';
+import FilterCell from 'ka-table/Components/FilterCell/FilterCell';
+import FilterRow from 'ka-table/Components/FilterRow/FilterRow';
+import FilterRowBoolean from 'ka-table/Components/FilterRowBoolean/FilterRowBoolean';
+import FilterRowDataType from 'ka-table/Components/FilterRowDataType/FilterRowDataType';
+import FilterRowDate from 'ka-table/Components/FilterRowDate/FilterRowDate';
+import FilterRowNumber from 'ka-table/Components/FilterRowNumber/FilterRowNumber';
+import FilterRowString from 'ka-table/Components/FilterRowString/FilterRowString';
+import GroupedColumnsRow from 'ka-table/Components/GroupedColumnsRow/GroupedColumnsRow';
+import GroupRow from 'ka-table/Components/GroupRow/GroupRow';
+import GroupRowContent from 'ka-table/Components/GroupRowContent/GroupRowContent';
+import GroupSummaryCell from 'ka-table/Components/GroupSummaryCell/GroupSummaryCell';
+import GroupSummaryRow from 'ka-table/Components/GroupSummaryRow/GroupSummaryRow';
+import HeadCell from 'ka-table/Components/HeadCell/HeadCell';
+import HeadCellContent from 'ka-table/Components/HeadCellContent/HeadCellContent';
+import HeadCellResize from 'ka-table/Components/HeadCellResize/HeadCellResize';
+import HeaderFilterButton from 'ka-table/Components/HeaderFilterButton/HeaderFilterButton';
+import HeadRow from 'ka-table/Components/HeadRow/HeadRow';
+import Loading from 'ka-table/Components/Loading/Loading';
+import NewRow from 'ka-table/Components/NewRow/NewRow';
+import NoDataRow from 'ka-table/Components/NoDataRow/NoDataRow';
+import Paging from 'ka-table/Components/Paging/Paging';
+import PagingIndex from 'ka-table/Components/PagingIndex/PagingIndex';
+import PagingPages from 'ka-table/Components/PagingPages/PagingPages';
+import PagingSize from 'ka-table/Components/PagingSize/PagingSize';
+import PagingSizes from 'ka-table/Components/PagingSizes/PagingSizes';
+import Popup from 'ka-table/Components/Popup/Popup';
+import PopupContent from 'ka-table/Components/PopupContent/PopupContent';
+import PopupContentItem from 'ka-table/Components/PopupContentItem/PopupContentItem';
+import Rows from 'ka-table/Components/Rows/Rows';
+import SummaryCell from 'ka-table/Components/SummaryCell/SummaryCell';
+import SummaryRow from 'ka-table/Components/SummaryRow/SummaryRow';
+import DefaultTable from 'ka-table/Components/Table/Table';
+import TableBody from 'ka-table/Components/TableBody/TableBody';
+import TableBodyContent from 'ka-table/Components/TableBodyContent/TableBodyContent';
+import TableFoot from 'ka-table/Components/TableFoot/TableFoot';
+import TableHead from 'ka-table/Components/TableHead/TableHead';
+import TablePaging from 'ka-table/Components/TablePaging/TablePaging';
+import TableWrapper from 'ka-table/Components/TableWrapper/TableWrapper';
+import VirtualizedRows from 'ka-table/Components/VirtualizedRows/VirtualizedRows';
+
 export { Table } from './Table';
+
+export const components = {
+  CellComponent,
+  CellEditor,
+  CellEditorBoolean,
+  CellEditorDataType,
+  CellEditorDate,
+  CellEditorNumber,
+  CellEditorState,
+  CellEditorString,
+  CellEditorValidation,
+  CellEditorValidationMessage,
+  CellText,
+  ColGroup,
+  DataAndDetailsRows,
+  DataRow,
+  DataRowContent,
+  DetailsRow,
+  EmptyCells,
+  FilterCell,
+  FilterRow,
+  FilterRowBoolean,
+  FilterRowDataType,
+  FilterRowDate,
+  FilterRowNumber,
+  FilterRowString,
+  GroupedColumnsRow,
+  GroupRow,
+  GroupRowContent,
+  GroupSummaryCell,
+  GroupSummaryRow,
+  HeadCell,
+  HeadCellContent,
+  HeadCellResize,
+  HeaderFilterButton,
+  HeadRow,
+  Loading,
+  NewRow,
+  NoDataRow,
+  Paging,
+  PagingIndex,
+  PagingPages,
+  PagingSize,
+  PagingSizes,
+  Popup,
+  PopupContent,
+  PopupContentItem,
+  Rows,
+  SummaryCell,
+  SummaryRow,
+  DefaultTable,
+  TableBody,
+  TableBodyContent,
+  TableFoot,
+  TableHead,
+  TablePaging,
+  TableWrapper,
+  VirtualizedRows,
+};
