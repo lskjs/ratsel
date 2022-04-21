da2bd6fe6243310ad0b50ebcebe7a2b40fc29cba
Andrey Kondalov <andrey@kondalov.com>
2022-04-21 13:23:53 +0400

    fix(button): pass ref

--- packages/button/src/Button.tsx
+++ packages/button/src/Button.tsx
@@ -1,5 +1,11 @@
-import type { FC, ReactNode } from 'react';
-import React, { SyntheticEvent, useState } from 'react';
+import React, {
+  FC,
+  forwardRef,
+  LegacyRef,
+  ReactNode,
+  SyntheticEvent,
+  useState,
+} from 'react';
 
 import { Base, BaseProps } from './components/Base';
 import { CheckIcon } from './components/CheckIcon';
@@ -21,89 +27,95 @@ interface ButtonState {
   status: 'error' | 'success' | null;
 }
 
-export const Button: FC<ButtonProps> = ({
-  children,
-  variant = 'primary',
-  bordered,
-  iconLeft,
-  iconRight,
-  icon,
-  disabled,
-  loading,
-  status,
-  onClick,
-  minWidth,
-  block,
-  ...props
-}) => {
-  const [state, setState] = useState<ButtonState>({
-    loading: false,
-    status: null,
-  });
+export const Button: FC<ButtonProps> = forwardRef(
+  (
+    {
+      children,
+      variant = 'primary',
+      bordered,
+      iconLeft,
+      iconRight,
+      icon,
+      disabled,
+      loading,
+      status,
+      onClick,
+      minWidth,
+      block,
+      ...props
+    },
+    ref,
+  ) => {
+    const [state, setState] = useState<ButtonState>({
+      loading: false,
+      status: null,
+    });
 
-  const handleSubmit = async (event: SyntheticEvent<HTMLElement>) => {
-    if (!onClick) return;
-    const promise = onClick(event);
-    if (isPromise(promise as unknown as Promise<() => unknown>)) {
-      setState({
-        ...state,
-        loading: true,
-      });
-      try {
-        await promise;
+    const handleSubmit = async (event: SyntheticEvent<HTMLElement>) => {
+      if (!onClick) return;
+      const promise = onClick(event);
+      if (isPromise(promise as unknown as Promise<() => unknown>)) {
         setState({
           ...state,
-          loading: false,
-          status: 'success',
+          loading: true,
         });
-      } catch (error) {
-        setState({
-          ...state,
-          loading: false,
-          status: 'error',
-        });
-      } finally {
-        setTimeout(() => {
+        try {
+          await promise;
           setState({
             ...state,
             loading: false,
-            status: null,
+            status: 'success',
           });
-        }, 1000);
+        } catch (error) {
+          setState({
+            ...state,
+            loading: false,
+            status: 'error',
+          });
+        } finally {
+          setTimeout(() => {
+            setState({
+              ...state,
+              loading: false,
+              status: null,
+            });
+          }, 1000);
+        }
       }
-    }
-  };
+    };
 
-  const loadingRender = loading || state.loading;
-  const statusRender = status || state.status;
-  const isOverlayRender = loadingRender || Boolean(statusRender);
-  const inverse = ['primary'].includes(variant);
+    const loadingRender = loading || state.loading;
+    const statusRender = status || state.status;
+    const isOverlayRender = loadingRender || Boolean(statusRender);
+    const inverse = ['primary'].includes(variant);
 
-  return (
-    <Base
-      {...props}
-      block={block}
-      variant={variant}
-      bordered={bordered}
-      iconLeft={icon || iconLeft}
-      iconRight={icon || iconRight}
-      disabled={disabled}
-      minWidth={minWidth}
-      loading={isOverlayRender}
-      onClick={isOverlayRender ? undefined : handleSubmit}
-    >
-      {isOverlayRender && (
-        <Overlay variant={variant} status={statusRender}>
-          {loadingRender && <Spinner inverse={inverse} />}
-          {!loadingRender && statusRender === 'error' && <CloseIcon />}
-          {!loadingRender && statusRender === 'success' && <CheckIcon />}
-        </Overlay>
-      )}
-      {iconLeft && <Icon>{iconLeft}</Icon>}
-      {!icon ? children : <Icon>{icon}</Icon>}
-      {iconRight && <Icon>{iconRight}</Icon>}
-    </Base>
-  );
-};
+    return (
+      <Base
+        {...props}
+        ref={ref as LegacyRef<HTMLButtonElement>}
+        block={block}
+        variant={variant}
+        bordered={bordered}
+        iconLeft={icon || iconLeft}
+        iconRight={icon || iconRight}
+        disabled={disabled}
+        minWidth={minWidth}
+        loading={isOverlayRender}
+        onClick={isOverlayRender ? undefined : handleSubmit}
+      >
+        {isOverlayRender && (
+          <Overlay variant={variant} status={statusRender}>
+            {loadingRender && <Spinner inverse={inverse} />}
+            {!loadingRender && statusRender === 'error' && <CloseIcon />}
+            {!loadingRender && statusRender === 'success' && <CheckIcon />}
+          </Overlay>
+        )}
+        {iconLeft && <Icon>{iconLeft}</Icon>}
+        {!icon ? children : <Icon>{icon}</Icon>}
+        {iconRight && <Icon>{iconRight}</Icon>}
+      </Base>
+    );
+  },
+);
 
 export default Button;
