a91b9279ab70ee61e51a7bcf8297ce11231308b7
Andrey Kondalov <andrey@kondalov.com>
2022-04-29 12:56:47 +0400

    chore(table): filter utils export

--- packages/table/src/utils.ts
+++ packages/table/src/utils.ts
@@ -1,7 +1,8 @@
 import * as kaCellUtils from 'ka-table/Utils/CellUtils';
 import * as kaComponentUtils from 'ka-table/Utils/ComponentUtils';
+import * as kaFilterUtils from 'ka-table/Utils/FilterUtils';
 import * as kaSortUtils from 'ka-table/Utils/SortUtils';
 
 export * from 'ka-table/utils';
 
-export { kaComponentUtils, kaSortUtils, kaCellUtils };
+export { kaComponentUtils, kaSortUtils, kaCellUtils, kaFilterUtils };
