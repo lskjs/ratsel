b2e2d783560dded04cd7cdf0967abd942a7e5814
Andrey Kondalov <andrey@kondalov.com>
2022-04-29 14:06:00 +0400

    fix(table): extend utils

--- packages/table/src/utils.ts
+++ packages/table/src/utils.ts
@@ -1,3 +1,4 @@
+import * as kaCellResizeUtils from 'ka-table/Utils/CellResizeUtils';
 import * as kaCellUtils from 'ka-table/Utils/CellUtils';
 import * as kaComponentUtils from 'ka-table/Utils/ComponentUtils';
 import * as kaFilterUtils from 'ka-table/Utils/FilterUtils';
@@ -5,4 +6,10 @@ import * as kaSortUtils from 'ka-table/Utils/SortUtils';
 
 export * from 'ka-table/utils';
 
-export { kaComponentUtils, kaSortUtils, kaCellUtils, kaFilterUtils };
+export {
+  kaComponentUtils,
+  kaSortUtils,
+  kaCellUtils,
+  kaFilterUtils,
+  kaCellResizeUtils,
+};
