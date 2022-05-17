ae3190fb7086dd4229e87a21fa533aa5b69f9b19
Andrey Kondalov <andrey@kondalov.com>
2022-05-17 21:32:45 +0400

    feat(table): ref support

--- packages/table/src/Table.tsx
+++ packages/table/src/Table.tsx
@@ -12,7 +12,12 @@ import {
   IHeadCellProps,
 } from 'ka-table/props';
 import { DispatchFunc } from 'ka-table/types';
-import React, { FC, useEffect, useState } from 'react';
+import React, {
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useState,
+} from 'react';
 
 import { globalFonts } from './components/globalFonts';
 import { Wrapper } from './components/Wrapper';
@@ -24,6 +29,10 @@ export interface ExtendedITableProps extends ITableProps {
   childComponents?: ITableAllProps['childComponents'];
 }
 
+export interface ITableState {
+  tableProps: ExtendedITableProps;
+}
+
 type ExternalReducer = (action: any, dispatch: DispatchFunc) => void;
 
 interface TableProps {
@@ -33,114 +42,121 @@ interface TableProps {
   onChange?: (arg: any) => any | Promise<any>;
 }
 
-export const Table: FC<TableProps> = ({
-  getReducer,
-  data,
-  onChangeState,
-  onChange,
-}) => {
-  const _data = deserialize(data);
-  if (getReducer && !_data.tableProps.singleAction) {
-    _data.tableProps.singleAction = true;
-  }
-  const [custom, changeCustom] = useState(_data.custom);
-  const [tableProps, changeTableProps] = useState(_data.tableProps);
+interface TableRef {
+  dispatch(action: any): void;
+  getState(): ITableState;
+}
 
-  function updateTableData(__data: ExtendedITableProps) {
-    const ___data = deserialize(__data);
-    if (getReducer && !___data.tableProps.singleAction) {
-      ___data.tableProps.singleAction = true;
+export const Table = forwardRef<TableRef, TableProps>(
+  ({ data, onChangeState, onChange }, ref) => {
+    const _data = deserialize(data);
+    // if (!_data.tableProps.singleAction) {
+    //   _data.tableProps.singleAction = true;
+    // }
+    const [tableState, changeTableState] = useState(_data);
+
+    function updateTableData(__data: ExtendedITableProps) {
+      const ___data = deserialize(__data);
+      // if (!___data.tableProps.singleAction) {
+      //   ___data.tableProps.singleAction = true;
+      // }
+      changeTableState(___data);
     }
-    changeCustom(___data.custom);
-    changeTableProps(___data.tableProps);
-  }
 
-  useEffect(() => {
-    updateTableData(data);
-  }, [data]);
+    useEffect(() => {
+      updateTableData(data);
+    }, [data]);
 
-  const reducer = async (__action: any, __dispatch: DispatchFunc) => {
-    if (__action?.type === 'UpdateTableData') {
-      const newData = await __action.updater({
-        ...tableProps,
-        custom,
+    const reducer = async (__action: any, __dispatch: DispatchFunc) => {
+      changeTableState((prevState: ITableState) => {
+        const newTableProps = kaReducer(prevState.tableProps, __action);
+        const newState = { ...prevState, tableProps: newTableProps };
+
+        if (onChangeState) onChangeState({ state: newState, action: __action });
+
+        if (__action?.type === 'UpdateTableData') {
+          const _newState = __action.updater(newState);
+          return _newState;
+        }
+
+        return newState;
       });
-      updateTableData(newData);
-      return;
+
+      if (onChange) await onChange({ action: __action, dispatch: __dispatch });
+    };
+
+    const dispatch: DispatchFunc = async (action) => {
+      reducer(action, dispatch);
+    };
+
+    const childComponents = {
+      ...(tableState.tableProps.childComponents || {}),
+      headCell: {
+        ...(tableState.tableProps.childComponents?.headCell || {}),
+        elementAttributes: (props: IHeadCellProps) =>
+          getStickyAttrs('thead', tableState.custom?.sticky, props.column),
+      },
+      cell: {
+        ...(tableState.tableProps.childComponents?.cell || {}),
+        elementAttributes: (props: ICellProps) =>
+          getStickyAttrs('tbody', tableState.custom?.sticky, props.column),
+      },
+      cellText: {
+        ...(tableState.tableProps.childComponents?.cellText || {}),
+        content: (props: ICellTextProps) =>
+          renderCustomComponent(
+            props,
+            tableState.custom?.cellViewComponents?.[props.column.key],
+          ),
+      },
+      cellEditor: {
+        ...(tableState.tableProps.childComponents?.cellEditor || {}),
+        content: (props: ICellEditorProps) =>
+          renderCustomComponent(
+            props,
+            tableState.custom?.cellEditorComponents?.[props.column.key],
+          ),
+      },
+    };
+
+    if (tableState.custom?.sticky?.summaryRow) {
+      childComponents.summaryRow = {
+        ...(tableState.tableProps.childComponents?.summaryRow || {}),
+        elementAttributes: () => ({
+          className: 'sticky-row-bottom',
+        }),
+      };
     }
 
-    changeTableProps((prevState: ExtendedITableProps) => {
-      const newState = kaReducer(prevState, __action);
-      if (onChangeState) onChangeState({ state: newState, action: __action });
-      return newState;
-    });
-
-    if (onChange) await onChange({ action: __action, dispatch: __dispatch });
-  };
-
-  const dispatch: DispatchFunc = async (action) => {
-    reducer(action, dispatch);
-    if (getReducer) {
-      getReducer(reducer);
+    if (tableState.custom?.cellTotalComponent) {
+      childComponents.summaryCell = {
+        ...(tableState.tableProps.childComponents?.summaryCell || {}),
+        content: tableState.custom?.cellTotalComponent,
+        elementAttributes: (props) =>
+          getStickyAttrs('summary', tableState.custom?.sticky, props.column),
+      };
     }
-  };
 
-  const childComponents = {
-    ...(tableProps.childComponents || {}),
-    headCell: {
-      ...(tableProps.childComponents?.headCell || {}),
-      elementAttributes: (props: IHeadCellProps) =>
-        getStickyAttrs('thead', custom?.sticky, props.column),
-    },
-    cell: {
-      ...(tableProps.childComponents?.cell || {}),
-      elementAttributes: (props: ICellProps) =>
-        getStickyAttrs('tbody', custom?.sticky, props.column),
-    },
-    cellText: {
-      ...(tableProps.childComponents?.cellText || {}),
-      content: (props: ICellTextProps) =>
-        renderCustomComponent(
-          props,
-          custom?.cellViewComponents?.[props.column.key],
-        ),
-    },
-    cellEditor: {
-      ...(tableProps.childComponents?.cellEditor || {}),
-      content: (props: ICellEditorProps) =>
-        renderCustomComponent(
-          props,
-          custom?.cellEditorComponents?.[props.column.key],
-        ),
-    },
-  };
+    useImperativeHandle(ref, () => ({
+      dispatch: (action: any) => {
+        reducer(action, dispatch);
+      },
+      getState: () => tableState,
+    }));
 
-  if (custom?.sticky?.summaryRow) {
-    childComponents.summaryRow = {
-      ...(tableProps.childComponents?.summaryRow || {}),
-      elementAttributes: () => ({
-        className: 'sticky-row-bottom',
-      }),
-    };
-  }
+    return (
+      <Wrapper
+        virtual={Boolean(tableState.tableProps.virtualScrolling?.enabled)}
+      >
+        <Global styles={globalFonts} />
+        <KaTable
+          {...tableState.tableProps}
+          dispatch={dispatch}
+          childComponents={childComponents}
+        />
+      </Wrapper>
+    );
+  },
+);
 
-  if (custom?.cellTotalComponent) {
-    childComponents.summaryCell = {
-      ...(tableProps.childComponents?.summaryCell || {}),
-      content: custom?.cellTotalComponent,
-      elementAttributes: (props) =>
-        getStickyAttrs('summary', custom?.sticky, props.column),
-    };
-  }
-
-  return (
-    <Wrapper virtual={Boolean(tableProps.virtualScrolling?.enabled)}>
-      <Global styles={globalFonts} />
-      <KaTable
-        {...tableProps}
-        dispatch={dispatch}
-        childComponents={childComponents}
-      />
-    </Wrapper>
-  );
-};
+Table.displayName = 'Table';
