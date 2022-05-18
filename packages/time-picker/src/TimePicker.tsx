b49d2f69a889eb7592100b235c102e2afffa8c69
Andrey Kondalov <andrey@kondalov.com>
2022-05-18 12:11:47 +0400

    fix(time-picker): ref support

--- packages/time-picker/src/TimePicker.tsx
+++ packages/time-picker/src/TimePicker.tsx
@@ -3,7 +3,7 @@ import { Moment } from 'moment';
 import RcTimePicker, {
   TimePickerProps as RcTimePickerProps,
 } from 'rc-time-picker';
-import React, { FC, useEffect, useState } from 'react';
+import React, { forwardRef, useEffect, useState } from 'react';
 
 import { base, popup } from './components/baseStyles';
 import { IconButton } from './components/IconButton';
@@ -26,96 +26,102 @@ interface CloseParams {
   open: false;
 }
 
-export const TimePicker: FC<TimePickerProps> = ({
-  defaultValue,
-  value: propValue,
-  onChange,
-  onClose,
-  submitOnClose,
-  clearable,
-  focusOnOpen,
-  withPositionUpdater,
-  ...props
-}) => {
-  const [value, setValue] = useState<Moment | undefined>(() => {
-    const _val = convertDateToMoment(value || defaultValue);
-    return _val;
-  });
+export const TimePicker = forwardRef<RcTimePicker, TimePickerProps>(
+  (
+    {
+      defaultValue,
+      value: propValue,
+      onChange,
+      onClose,
+      submitOnClose,
+      clearable,
+      focusOnOpen,
+      withPositionUpdater,
+      ...props
+    },
+    ref,
+  ) => {
+    const [value, setValue] = useState<Moment | undefined>(() => {
+      const _val = convertDateToMoment(value || defaultValue);
+      return _val;
+    });
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      document
-        .querySelector('.ratsel-tp-input')
-        ?.setAttribute('readonly', 'true');
-    }
-  }, []);
-
-  useEffect(() => {
-    if (!defaultValue) {
-      setValue(convertDateToMoment(propValue));
-    }
-  }, [propValue]);
-
-  const handleChange = (newValue: Moment) => {
-    if (withPositionUpdater) {
-      updatePanelPosition(true);
-    }
-    setValue(newValue);
-    if (!submitOnClose && onChange) onChange(newValue?.toDate());
-  };
-
-  const handleClose = (newState: CloseParams) => {
-    if (onClose) onClose(newState);
-    if (submitOnClose && onChange && value) onChange(value.toDate());
-  };
-
-  const handleOpen = () => {
-    if (withPositionUpdater) {
-      updatePanelPosition();
-    }
-    if (focusOnOpen) {
-      setTimeout(() => {
+    useEffect(() => {
+      if (typeof window !== 'undefined') {
         document
-          .querySelector<HTMLInputElement>('.ratsel-tp-panel-input')
-          ?.focus();
-      }, 0);
-    }
-  };
+          .querySelector('.ratsel-tp-input')
+          ?.setAttribute('readonly', 'true');
+      }
+    }, []);
 
-  const handleClear = () => {
-    setValue(undefined);
-  };
+    useEffect(() => {
+      if (!defaultValue) {
+        setValue(convertDateToMoment(propValue));
+      }
+    }, [propValue]);
 
-  return (
-    <ClassNames>
-      {({ css, theme }) => (
-        <RcTimePicker
-          showSecond={false}
-          {...props}
-          value={value}
-          onChange={handleChange}
-          onOpen={handleOpen}
-          onClose={handleClose}
-          prefixCls="ratsel-tp"
-          focusOnOpen={focusOnOpen}
-          className={css(base(theme as Theme))}
-          popupClassName={css(popup(theme as Theme))}
-          inputIcon={
-            <IconButton as="span" order={2}>
-              <ClockIcon />
-            </IconButton>
-          }
-          clearIcon={
-            clearable ? (
-              <IconButton onClick={handleClear} order={1}>
-                <CloseIcon />
+    const handleChange = (newValue: Moment) => {
+      if (withPositionUpdater) {
+        updatePanelPosition(true);
+      }
+      setValue(newValue);
+      if (!submitOnClose && onChange) onChange(newValue?.toDate());
+    };
+
+    const handleClose = (newState: CloseParams) => {
+      if (onClose) onClose(newState);
+      if (submitOnClose && onChange && value) onChange(value.toDate());
+    };
+
+    const handleOpen = () => {
+      if (withPositionUpdater) {
+        updatePanelPosition();
+      }
+      if (focusOnOpen) {
+        setTimeout(() => {
+          document
+            .querySelector<HTMLInputElement>('.ratsel-tp-panel-input')
+            ?.focus();
+        }, 0);
+      }
+    };
+
+    const handleClear = () => {
+      setValue(undefined);
+    };
+
+    return (
+      <ClassNames>
+        {({ css, theme }) => (
+          <RcTimePicker
+            ref={ref}
+            showSecond={false}
+            {...props}
+            value={value}
+            onChange={handleChange}
+            onOpen={handleOpen}
+            onClose={handleClose}
+            prefixCls="ratsel-tp"
+            focusOnOpen={focusOnOpen}
+            className={css(base(theme as Theme))}
+            popupClassName={css(popup(theme as Theme))}
+            inputIcon={
+              <IconButton as="span" order={2}>
+                <ClockIcon />
               </IconButton>
-            ) : null
-          }
-        />
-      )}
-    </ClassNames>
-  );
-};
+            }
+            clearIcon={
+              clearable ? (
+                <IconButton onClick={handleClear} order={1}>
+                  <CloseIcon />
+                </IconButton>
+              ) : null
+            }
+          />
+        )}
+      </ClassNames>
+    );
+  },
+);
 
-export default TimePicker;
+TimePicker.displayName = 'TimePicker';
